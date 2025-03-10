9cb228dc7918022d38e7f0766d3b9e52d03ed7df
greg-Da <gregoiredalmasso@gmail.com>
2025-03-10 17:01:45 +0100

    test

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dfeve <dfeve@student.42.fr>                +#+  +:+       +#+        */
+/*   By: greg <greg@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:46:40 by dfeve             #+#    #+#             */
-/*   Updated: 2025/03/06 16:32:24 by dfeve            ###   ########.fr       */
+/*   Updated: 2025/03/10 17:01:38 by greg             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ int	main(int argc, char **argv, char **env)
 
 	(void)argc;
 	(void)argv;
-	toeknized = create_token_struct("$1ahahaha$hey$USER");
-	print_tab(toeknized->split_input);
-	print_tokens(toeknized->tokens);
-	expand(toeknized, &env, env);
-	print_tab(toeknized->split_input);
-	free_token_struct(toeknized);
+	printf("Hello, World!\n");
+// 	toeknized = create_token_struct("$1ahahaha$hey$USER");
+// 	print_tab(toeknized->split_input);
+// 	print_tokens(toeknized->tokens);
+// 	expand(toeknized, &env, env);
+// 	print_tab(toeknized->split_input);
+// 	free_token_struct(toeknized);
 }
